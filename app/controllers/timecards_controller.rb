9f14d2b9ba26a4874c785daedfd16e6c60229613
ChikaraK <nowano0403@gmail.com>
2019-07-07 12:31:17 +0000

    add time config

--- app/controllers/timecards_controller.rb
+++ app/controllers/timecards_controller.rb
@@ -1,8 +1,15 @@
 class TimecardsController < ApplicationController
-	
+
 	def index
 	end
 
 	def show
+		# 出勤記録済確認用
+		# @timecard = Timecard.find(params[:id])
+	end
+
+	def create
+		# 出勤記録用
+		@newcard = Timecard.new
 	end
 end
