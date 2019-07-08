9866ff332b169632f0b5cbc322a42840242ed2ff
ChikaraK <nowano0403@gmail.com>
2019-07-08 07:31:28 +0000

    出退勤の最低限作成

--- app/controllers/timecards_controller.rb
+++ app/controllers/timecards_controller.rb
@@ -1,15 +1,30 @@
 class TimecardsController < ApplicationController
-
 	def index
 	end
 
 	def show
-		# 出勤記録済確認用
-		# @timecard = Timecard.find(params[:id])
+		# 出退勤記録済確認用
+		@newcard = Timecard.new
+		@timecard = Timecard.find(params[:id])
 	end
 
 	def create
 		# 出勤記録用
-		@newcard = Timecard.new
+		newcard = Timecard.new(timecard_params)
+		newcard.save
+		redirect_to timecards_path
+	end
+
+	def update
+		# 退勤記録用
+		timecard = Timecard.find(params[:id])
+		timecard.update(timecard_params)
+		redirect_to timecards_path
+	end
+
+	private
+
+	def timecard_params
+		params.require(:timecard).permit(:year,:month,:day,:in_time,:out_time,:user_id)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_scope :user do
   	root to: "devise/sessions#new"
   end
-  resources :timecards, only:[:index,:show,:create]
+  resources :timecards, only:[:index,:show,:create,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
